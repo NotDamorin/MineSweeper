293babfb6b5b210d166550305928c2f39ee84c0a
Damorin <damorinx@gmail.com>
2014-01-12 14:46:22 +0100

    Initial project creation and first test for TDD

---
+++ src/test/GameStartupTests.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import src.GameBoard;
+
+/**
+ * @author Damien
+ *
+ */
+public class GameStartupTests {
+
+	@Test
+	public void createBoardTest() {
+		GameBoard gameBoard = new GameBoard();
+	}
+
+}
